af8f9ea810d1ee1e7ccf98475f5e8995aa0d5b3a
sannysc <sannysherief@gmail.com>
2019-08-26 15:45:24 -0700

    Yup validation for registration.

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,31 +1,32 @@
 import React, { useState, useContext, useEffect } from 'react';
 import AuthContext from '../../context/auth/authContext';
-import { Form, Field, ErrorMessage, withFormik } from 'formik';
-import { Form as SemanticForm, Button, Container } from 'semantic-ui-react';
+import { Form, Field, withFormik } from 'formik';
+import { Form as SemanticForm, Button, Grid, Message } from 'semantic-ui-react';
 import * as yup from 'yup';
+import _ from 'lodash';
 
-const Registration = props => {
+const Registration = ({ errors, touched, values, status, history }) => {
 	const authContext = useContext(AuthContext);
 
 	const { register, isAuthenticated } = authContext;
 
 	useEffect(() => {
 		if (isAuthenticated) {
-			props.history.push('/users/dashboard');
+			history.push('/users/dashboard');
 		}
 		// eslint-disable-next-line
-	}, [isAuthenticated, props.history]);
+	}, [isAuthenticated, history]);
 
-	const [user, setUser] = useState({
-		fname: '',
-		lname: '',
-		useremail: '',
-		username: '',
-		password: '',
-		password2: ''
-	});
+	// const [user, setUser] = useState({
+	// 	fname: '',
+	// 	lname: '',
+	// 	useremail: '',
+	// 	username: '',
+	// 	password: '',
+	// 	password2: ''
+	// });
 
-	const { fname, lname, useremail, username, password, password2 } = user;
+	// const { fname, lname, useremail, username, password, password2 } = user;
 
 	// const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
 
@@ -47,47 +48,58 @@ const Registration = props => {
 	// };
 
 	return (
-		<Container>
-			<Form className="ui form">
-				<SemanticForm.Field>
-					<label>
-						First Name
-          <Field type="text" name="fname" />
-					</label>
-				</SemanticForm.Field>
-				<SemanticForm.Field>
-					<label>
-						Last Name
-          <Field type="text" name="lname" />
-					</label>
-				</SemanticForm.Field>
-				<SemanticForm.Field>
-					<label>
-						Email
-          <Field type="email" name="useremail" />
-					</label>
-				</SemanticForm.Field>
-				<SemanticForm.Field>
-					<label>
-						Username
-          <Field type="text" name="username" />
-					</label>
-				</SemanticForm.Field>
-				<SemanticForm.Field>
-					<label>
-						Password
-          <Field type="password" name="password" />
-					</label>
-				</SemanticForm.Field>
-				<SemanticForm.Field>
-					<label>
-						Confirm Password
-          <Field type="password" name="password2" />
-					</label>
-				</SemanticForm.Field>
-				<Button primary type="submit">Register</Button>
-			</Form>
-		</Container>
+		<Grid container centered>
+			<Grid.Column width={8}>
+				<Form className="ui form">
+					{
+						!_.isEmpty(_.intersection(Object.keys(touched), Object.keys(errors))) && (
+							<Message
+								icon="warning circle" header="There's some problems with the information below."
+								negative list={_.intersection(Object.keys(touched), Object.keys(errors)).map(key => errors[key])}
+							/>
+						)
+					}
+
+					<SemanticForm.Field>
+						<label>
+							First Name
+          		<Field type="text" name="fname" />
+						</label>
+					</SemanticForm.Field>
+					<SemanticForm.Field>
+						<label>
+							Last Name
+          		<Field type="text" name="lname" />
+						</label>
+					</SemanticForm.Field>
+					<SemanticForm.Field>
+						<label>
+							Email
+          		<Field type="email" name="useremail" />
+						</label>
+					</SemanticForm.Field>
+					<SemanticForm.Field>
+						<label>
+							Username
+          		<Field type="text" name="username" />
+						</label>
+					</SemanticForm.Field>
+					<SemanticForm.Field>
+						<label>
+							Password
+          		<Field type="password" name="password" />
+						</label>
+					</SemanticForm.Field>
+					<SemanticForm.Field>
+						<label>
+							Confirm Password
+          		<Field type="password" name="password2" />
+						</label>
+					</SemanticForm.Field>
+					<Button primary type="submit">Register</Button>
+				</Form>
+			</Grid.Column>
+		</Grid>
 		// <div className='form-container'>
 		// 	<h1>
 		// 		Account <span>Register</span>
@@ -164,24 +176,29 @@ const Registration = props => {
 };
 
 const FormikRegistration = withFormik({
-	mapPropsToValues: ({ name, email, password, tos }) => {
+	mapPropsToValues: ({ fname, lname, useremail, username, password, password2 }) => {
 		return {
-			name: name || '',
-			email: email || '',
+			fname: fname || '',
+			lname: lname || '',
+			useremail: useremail || '',
+			username: username || '',
 			password: password || '',
-			tos: tos || false,
+			password2: password2 || '',
 		};
 	},
 
 	validationSchema: yup.object().shape({
-		name: yup.string().required('You must provide a name.').max(10, 'Your name cannot be longer than 10 characters.'),
-		email: yup.string().email('The email provided is not valid.').required('You must provide an email.'),
-		password: yup.string().required('You must set a password.').min(6, 'Your password must be at least 6 characters long.'),
-		tos: yup.boolean().required().oneOf([true], 'You must agree to the terms of service.'),
+		fname: yup.string().required("You must provide your first name."),
+		lname: yup.string().required("You must provide your last name."),
+		useremail: yup.string().required("You must provide an email.").email("The email provided is not valid."),
+		username: yup.string().required("You must provide a username.").min(5, "Your username must be at least 5 characters long."),
+		password: yup.string().required("You must provide a password.").min(6, "Your password must be at least 6 characters long."),
+		password2: yup.string().required("You must confirm your password.").oneOf([yup.ref('password')], "The passwords do not match.")
 	}),
 
-	handleSubmit: function (values, { props, resetForm }) {
-
+	handleSubmit: function ({ fname, lname, useremail, username, password }, { resetForm }) {
+		console.log({ fname, lname, useremail, username, password });
+		resetForm();
 	}
 })(Registration);
 
