135ecf9629aed7c8515e1b4580b734819a3948ee
Thai Nguyen <discoverylight@yahoo.com>
2019-08-26 14:18:06 -0700

    added PrivateRoute component, added a StaffDashboard and a UserDashboard

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import AuthState from './context/auth/AuthState';
+import AuthState from "./context/auth/AuthState";
 
-import Login from './components/auth/Login';
-import Home from './components/pages/Home';
-import Register from './components/auth/Registration';
+import Login from "./components/auth/Login";
+import Home from "./components/pages/Home";
+import Register from "./components/auth/Registration";
 
-import './App.css';
+//Routing
+import PrivateRoute from "./components/routing/PrivateRoute";
+
+//User Routing
+import UserDashboard from "./components/pages/User/Dashboard";
+
+import "./App.css";
 
 function App() {
-	return (
-		<AuthState>
-			<div className='App'>
-				APP
-				<Router>
-					<Fragment>
-						<Switch>
-							<Route exact path='/' component={Home} />
-							<Route exact path='/register' component={Register} />
-							<Route exact path='/login' component={Login} />
-						</Switch>
-					</Fragment>
-				</Router>
-			</div>
-		</AuthState>
-	);
+  return (
+    <AuthState>
+      <div className="App">
+        APP
+        <Router>
+          <Fragment>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <PrivateRoute path="/users/dashboard" component={UserDashboard} />
+            </Switch>
+          </Fragment>
+        </Router>
+      </div>
+    </AuthState>
+  );
 }
 
 export default App;
