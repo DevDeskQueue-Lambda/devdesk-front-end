958334f13b0d2cf423ca5ae707a06ce42bdf98b8
sannysc <sannysherief@gmail.com>
2019-08-26 14:08:21 -0700

    Add Semantic UI.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'fomantic-ui-css/semantic.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
