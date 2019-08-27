c5de970cfa4ad928493be5c75bff75462d3b95e2
sannysc <sannysherief@gmail.com>
2019-08-27 13:46:48 -0700

    Login updates.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,61 +1,69 @@
 import React, { useState, useContext, useEffect } from "react";
 import AuthContext from "../../context/auth/authContext";
+import { Form, Field, withFormik } from 'formik';
+import { } from 'semantic-ui-react';
+import { Form as SemanticForm, Button, Grid, Header, Message } from 'semantic-ui-react';
+import * as yup from 'yup';
 
-const Login = props => {
+const Login = ({ values, history, status }) => {
   const authContext = useContext(AuthContext);
 
-  const { login, isAuthenticated, userInfo } = authContext;
+  const { login, isAuthenticated, userInfo, error } = authContext;
+  const { username, password } = values;
 
   useEffect(() => {
+    if (status) {
+      login({ username, password });
+    }
+
     if (isAuthenticated) {
       if (userInfo && userInfo.userRoles.length > 1) {
-        props.history.push("/dashboard");
+        history.push("/dashboard");
       } else {
         const role = userInfo.userRoles[0].role.name;
 
-        props.history.push(`/${role}/dashboard`);
+        history.push(`/${role}/dashboard`);
       }
     }
-  }, [isAuthenticated, props.history, userInfo]);
-
-  const [user, setUser] = useState({
-    username: "",
-    password: ""
-  });
-
-  const { username, password } = user;
-
-  function handleChanges(e) {
-    setUser({ ...user, [e.target.name]: e.target.value });
-  }
-
-  const onSubmit = e => {
-    e.preventDefault();
-    login({ username, password });
-  };
+  }, [isAuthenticated, history, userInfo, status]);
 
   return (
-    <div>
-      <h1>User Login</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="username"
-          value={user.username}
-          onChange={handleChanges}
-        />
-        <input
-          type="text"
-          name="password"
-          placeholder="password"
-          value={user.password}
-          onChange={handleChanges}
-        />
-        <button>Login</button>
-      </form>
-    </div>
+    <Grid container centered>
+      <Grid.Column width={5}>
+        <Form className="ui form">
+          {
+            error && <Message negative icon="warning circle" content={error.data.error_description} />
+          }
+          <Header as="h1">User Login</Header>
+          <SemanticForm.Field>
+            <Field type="text" name="username" placeholder="Username" />
+          </SemanticForm.Field>
+          <SemanticForm.Field>
+            <Field type="password" name="password" placeholder="Password" />
+          </SemanticForm.Field>
+          <Button primary type="submit">Login</Button>
+        </Form>
+      </Grid.Column>
+    </Grid>
   );
 };
 
-export default Login;
+const FormikLogin = withFormik({
+  mapPropsToValues: function ({ username, password }) {
+    return {
+      username: username || '',
+      password: password || '',
+    };
+  },
+
+  validationSchema: yup.object().shape({
+    username: yup.string().required('You must provide your username.'),
+    password: yup.string().required('You must provide your password.'),
+  }),
+
+  handleSubmit: function (values, { setStatus }) {
+    setStatus(values);
+  },
+})(Login);
+
+export default FormikLogin;
