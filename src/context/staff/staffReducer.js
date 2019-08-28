1f3041d8b102e5323cc34d587f5f090ff9fb0d6b
Brandon Desselle <31125685+KeKoolKat97@users.noreply.github.com>
2019-08-28 11:56:36 -0500

    Update staffReducer.js

--- src/context/staff/staffReducer.js
+++ src/context/staff/staffReducer.js
@@ -1,30 +1,36 @@
-//! Login / Logout
-// Staff Member can - Login/Logout
-//! Tickets
-// Staff Member can - Claim/Unclaim a ticket
-// Staff Member can - Change a ticket's status
-// Staff Member can - Comment/Edit a comment on a ticket
-
-import { CLAIM_TICKET, UNCLAIM_TICKET, EDIT_TICKET_STATUS, ADD_TICKET_COMMENT, EDIT_TICKET_COMMENT } from '../types';
+import {
+  GET_UNASSIGNED_TICKETS,
+  ASSIGN_TICKET,
+  UNASSIGN_TICKET,
+  EDIT_TICKET_STATUS,
+  SET_LOADING,
+  STAFF_ERROR
+} from "../types";
 
 export default (state, action) => {
   console.log("reducer action", action);
   switch (action.type) {
-    case CLAIM_TICKET:
-      console.log('CLAIM_TICKET');
+    case GET_UNASSIGNED_TICKETS:
+      console.log('GET_UNASSIGNED_TICKETS');
+      return {
+        ...state,
+        tickets: action.payload,
+        loading: false
+      };
+    case ASSIGN_TICKET:
+      console.log('ASSIGN_TICKET');
       return {};
-    case UNCLAIM_TICKET:
-        console.log('UNCLAIM_TICKET');
-        return {};
+    case UNASSIGN_TICKET:
+      console.log('UNASSIGN_TICKET');
+      return {};
     case EDIT_TICKET_STATUS:
-        console.log('EDIT_TICKET_STATUS');
-        return {};
-    case ADD_TICKET_COMMENT:
-        console.log('ADD_TICKET_COMMENT');
-        return {};
-    case EDIT_TICKET_COMMENT:
-        console.log('EDIT_TICKET_COMMENT');
-        return {};
+      console.log('EDIT_TICKET_STATUS');
+      return {};
+    case STAFF_ERROR:
+      return {
+        ...state,
+        error: action.payload
+      };
     default:
       return state;
   }
