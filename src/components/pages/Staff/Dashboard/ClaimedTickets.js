f6a8d2f65b26e8a93830e1222e0721ecaf92d611
Brandon Desselle <31125685+KeKoolKat97@users.noreply.github.com>
2019-08-28 17:36:20 -0500

    Create ClaimedTickets.js

---
+++ src/components/pages/Staff/Dashboard/ClaimedTickets.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Button, Header, Grid, Label, Modal, Table } from "semantic-ui-react";
+import StaffContext from '../../../../context/staff/staffContext'
+
+export default function ClaimedTickets(props) {
+  const staffContext = React.useContext(StaffContext);
+  const { user, tickets, fetchAssignedTickets, fetchCurrentUserData } = staffContext;
+
+  React.useEffect(() => {
+    fetchAssignedTickets();
+    fetchCurrentUserData();
+  }, []);
+
+  /* const filteredTickets = tickets.assignedid.userid.filter(ticketid => ticketid < user.userid); */
+  const mappedTickets = () => {
+    tickets.map(ticket => console.log(ticket.assigneduser))
+  }
+
+  console.log("USER ID", user.userid);
+  console.log("TICKETS", tickets);
+  console.log('MAPPED TICKETS', mappedTickets());
+  /* console.log('FILTERED TICKETS', filteredTickets); */
+
+  return (
+    <div>
+      <Grid padded="horizontally" style={{ marginTop: "5rem" }}>
+        <Grid.Row columns={2}>
+          <Grid.Column>
+            <Header>My Claimed Tickets</Header>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Column width={16}>
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Category</Table.HeaderCell>
+                <Table.HeaderCell>Title</Table.HeaderCell>
+                <Table.HeaderCell>Description</Table.HeaderCell>
+                <Table.HeaderCell>Tried</Table.HeaderCell>
+                <Table.HeaderCell>Status</Table.HeaderCell>
+                <Table.HeaderCell>Actions</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {tickets &&
+                tickets.length > 0 &&
+                tickets.map(ticket => (
+                  <Table.Row key={ticket.ticketid}>
+                    <Table.Cell>
+                      {ticket.ticketCategories &&
+                        ticket.ticketCategories.length > 0 &&
+                        ticket.ticketCategories.map(category => (
+                          <Label key={category.category.categoryid}>
+                            {category.category.name}
+                          </Label>
+                        ))}
+                    </Table.Cell>
+                    <Table.Cell>{ticket.title}</Table.Cell>
+                    <Table.Cell>{ticket.description}</Table.Cell>
+                    <Table.Cell>{ticket.tried}</Table.Cell>
+                    <Table.Cell>{ticket.status.name}</Table.Cell>
+                    <Table.Cell>
+                      <Button /* onClick={() => handleTicketModal("edit")} */>
+                        View
+                      </Button>
+                      <Button
+                        color="red"
+                        /* onClick={() => handleTicketModal("delete")} */
+                      >
+                        Unclaim
+                      </Button>
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+            </Table.Body>
+          </Table>
+        </Grid.Column>
+      </Grid>
+    </div>
+  )
+}
