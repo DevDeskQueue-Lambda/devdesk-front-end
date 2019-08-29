e59611c1a6a5c43f7861dca8114a23ba3eae46ae
Brandon Desselle <desselle.brandon2@gmail.com>
2019-08-29 14:31:07 -0500

    Update index.js

--- src/components/pages/Staff/Dashboard/index.js
+++ src/components/pages/Staff/Dashboard/index.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { Header, Grid, Table, Label } from "semantic-ui-react";
-import { axiosWithAuth } from '../../../../utils';
+import { axiosWithAuth } from "../../../../utils";
 
 const StaffDashboard = () => {
   const [tickets, setTickets] = useState([]);
 
   useEffect(() => {
-    axiosWithAuth().get('/tickets/alltickets')
+    axiosWithAuth()
+      .get("/tickets/alltickets")
       .then(response => setTickets(response.data));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-const StaffDashboard = props => {
   return (
-    <Grid container style={{ marginTop: '2rem' }}>
+    <Grid container style={{ marginTop: "2rem" }}>
       <Grid.Column width={16}>
         <Header as="h1">Staff Dashboard</Header>
       </Grid.Column>
@@ -29,28 +29,32 @@ const StaffDashboard = props => {
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {tickets && tickets.map(ticket => {
-              return (
-                <Table.Row key={ticket.ticketid}>
-                  <Table.Cell>
-                    <Link to={`/staff/dashboard/ticket/${ticket.ticketid}`} style={{fontWeight: 'bold'}}>
-                      {ticket.title}
-                    </Link></Table.Cell>
-                  <Table.Cell>{ticket.description}</Table.Cell>
-                  <Table.Cell>
-                    <Label.Group>
-                      {
-                        ticket.ticketCategories.map(category => (
+            {tickets &&
+              tickets.map(ticket => {
+                return (
+                  <Table.Row key={ticket.ticketid}>
+                    <Table.Cell>
+                      <Link
+                        to={`/staff/dashboard/ticket/${ticket.ticketid}`}
+                        style={{ fontWeight: "bold" }}
+                      >
+                        {ticket.title}
+                      </Link>
+                    </Table.Cell>
+                    <Table.Cell>{ticket.description}</Table.Cell>
+                    <Table.Cell>
+                      <Label.Group>
+                        {ticket.ticketCategories.map(category => (
                           <Label key={category.category.categoryid}>
                             {category.category.name}
                           </Label>
                         ))}
-                    </Label.Group>
-                  </Table.Cell>
-                  <Table.Cell>{ticket.status.name}</Table.Cell>
-                </Table.Row>
-              )
-            })}
+                      </Label.Group>
+                    </Table.Cell>
+                    <Table.Cell>{ticket.status.name}</Table.Cell>
+                  </Table.Row>
+                );
+              })}
           </Table.Body>
         </Table>
       </Grid.Column>
